eaf79c8ef401ada601a418f0cdb735fc1efd1606
System Error <LOGAN2413@yandex.ru>
2024-04-02 22:40:24 +0300

    new_vk_api

--- main.py
+++ main.py
@@ -1,103 +1,124 @@
-import os
-import sys
-import requests, json, time, os
+import requests, json, time, os, sys 
+from vk_save import url_api, download, info_max_count, test_api_json
+# from dotenv import load_dotenv
+import tempfile, base64, zlib
+from tkinter.messagebox import showinfo
+from tkinter import *
+from tkinter import ttk 
+from tkinter import filedialog
+from pywinauto import Desktop
+import tempfile, base64, zlib
+import threading
+import keyboard
+import webbrowser
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
-from main_untitled import Ui_Dialog
-from vk_save import url_api, download, info_max_count, test_api
-# from async_one import main_asyncio
-from dotenv import load_dotenv
+# load_dotenv()
 
-load_dotenv()
-VK_USER_ID = os.getenv('VK_USER_ID')
-VK_TOKEN = os.getenv('VK_TOKEN')
+# input_id = os.getenv('VK_USER_ID')
+# input_token = os.getenv('VK_TOKEN')
+value_var = 0
+entry = None
 
+def token():
+    oauth = webbrowser.open('https://oauth.vk.com/authorize?client_id=51636410&display=page&redirect_uri=https://ouath.vk.com/blank.html&scope=photos&response_type=token&v=5.131')
 
-class ExampleApp(QtWidgets.QDialog):
-    def __init__(self):
-        super(ExampleApp, self).__init__()
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self) 
-        button = self.ui.buttonBox.clicked.connect(self.response)
+    global entry 
+    entry = ttk.Entry()
+    entry.pack(anchor=NW, padx=6, pady=6, fill=X)
+
+    btn = ttk.Button(text="Ok", command=check_all_Photos)
+    btn.pack(anchor=CENTER)
+
+def check_all_Photos():
+    # global input_token
+    # global input_id
+    link_ent_1 = entry.get()
+    if link_ent_1 != None:
+        try:
+            link_ent_2 = link_ent_1.split("/")[-1]
+            link_ent_3 = link_ent_2.split("#")[-1]
+
+            link_ent_token1 = link_ent_3.split("&")[0]
+            link_ent_token = link_ent_token1.split('=')[-1]
+
+            link_ent_id1 = link_ent_3.split("&")[-1]
+            link_ent_id = link_ent_id1.split('=')[-1]
+
+            input_id = link_ent_id
+            input_token = link_ent_token
+            
+            label_input_id_token = ttk.Label(text=(('id:'),input_id))
+            label_input_id_token.pack(anchor=NW, padx=6, pady=6)
+            
+            process_1 = threading.Thread(target=process_one,  args=(input_id, input_token))
+            process_1.start()
+            
+        except:
+            showinfo(title="Erorr", message="error validator2")
+    else:
+        showinfo(title="Erorr", message="error validator1")
+    
+
+def process_one(input_id, input_token):
+    response_count_1 = url_api(0, 1, input_id, input_token)
+    response_jsons = test_api_json(response_count_1)
+
+    if response_jsons != False:
+        max_count = info_max_count(response_count_1)
+        label_max_count_photos = ttk.Label(text=(('Фото:'), max_count))
+        label_max_count_photos.pack(anchor=NW, padx=6, pady=6)
+
+        btn_select_ = ttk.Button(text="Выбрать", command=lambda:btn_download(input_id, input_token, max_count))
+        btn_select_.pack(side=BOTTOM)
         
-    def response(self):  
-            # input_id = self.ui.lineEdit.text()
-            # input_token = self.ui.lineEdit_2.text()
-            input_count = self.ui.lineEdit_3.text()
+    else:
+        showinfo(title="Erorr", message="Токен Устарел")
 
-            self.count_offset = 0
-            self.response_jsons = url_api(0, 1, VK_USER_ID, VK_TOKEN)
-            self.max_count = info_max_count(self.response_jsons)
-            self.count_in = test_api(self.response_jsons)
+def btn_download(input_id, input_token, max_count):
+    offset = 0
+    process_2 = threading.Thread(target=process_two, args=(input_id, input_token, max_count, offset))
+    process_2.start()
 
-            if self.count_in != False:
-                # в принципе все логично но у PYQT, есть встроенные функции преобразования "строки состояния" в проценты от шагов минимального и максимального шага 
-                # тут инфа по ней https://doc.qt.io/qt-5/qprogressbar.html#details
-                print(self.max_count)
-                # self.main_asyncio = main_asyncio(self.count_in, self.max_count, self.response_jsons, self.count_offset)
-                # print(self.main_asyncio)
+def process_two(input_id, input_token, max_count, offset):
+    filepath = 'C:/Users/LORD2/Pictures/img_vk'
+    btn_exit_ = ttk.Button(text="Завершить",)
+    btn_exit_.pack(side=BOTTOM)
+    if filepath != "":
+        label_save_file = ttk.Label(text=filepath)
+        label_save_file.pack(side=BOTTOM)
 
-                for self.count_in in range(0, self.max_count):
-                    self.count_in+=1
-                    self.response_jsons_one = url_api(self.count_offset, self.count_in, VK_USER_ID, VK_TOKEN)
-                    self.downloads = download(self.response_jsons_one)
-
-                    if self.count_in == 200:
-                        self.count_offset == self.count_in
-                    print("count_in", self.count_in, "count_offset", self.count_offset)
-
-                    self.result_max_count = self.max_count / self.count_in
-                    self.percentages = 100 / self.result_max_count
-                    self.ui.progressBar.setValue(int(self.percentages))
-                    print(self.count_in)
-                    
-
-
-
-                # while self.count_in != self.max_count:
-                #     self.response_jsons_one = url_api(self.count_offset, self.count_in, VK_USER_ID, VK_TOKEN)
-                #     self.downloads = download(self.response_jsons_one)
-                        
-                #     if self.count_in == self.count_offset + self.count_in:
-                #         self.count_offset == self.count_in
-                #     print("count_in", self.count_in, "count_offset", self.count_offset)
-
-                    
-                
+        for i in range(max_count):
+            print(i)
+            response_count_max = url_api(offset, 1, input_id, input_token)
+            download_img = download(response_count_max, filepath)
+            offset += 1
+            if keyboard.is_pressed('q'):
+                continue
             else:
-                msg = QMessageBox() 
-                msg.setIcon(QMessageBox.Information)  
-                msg.setWindowTitle("Warning")
-                msg.setText("что-то пошло не так")
-                msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel) 
-                retval = msg.exec_()
-        # except:
-        #     msg = QMessageBox() 
-        #     msg.setIcon(QMessageBox.Information)  
-        #     msg.setWindowTitle("Warning")
-        #     msg.setText("Проблемы с соединением")
-        #     msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel) 
-        #     retval = msg.exec_()
-        # print("no")
-        # self.downloads = download(self.response_json, 0 )
+                if download_img == 1:
+                    value_var = offset / max_count * 100
+                    progressbar.configure(value = round(value_var))
+                    progressbar.update()
+                    print(offset)
+                else:
+                    showinfo(title="Info", message="Ошибка-2")
+                    break
+        print(offset)
+        return filepath
+    else:
+        print("Директория не выбрана")
+    
 
-        # async def main_asyncio(self, self.count_in, self.max_count, self.response_jsons, self.count_offset):
-        #     print("ffff")
-        #     self.response_jsons_one = url_api(self.count_offset, self.count_in, VK_USER_ID, VK_TOKEN)
-        #     self.downloads = download(self.response_jsons_one)
-                
-        #     if self.count_in == self.count_offset + self.count_in:
-        #         self.count_offset == self.count_in
-        #     print("count_in", self.count_in, "count_offset", self.count_offset)
-        # return   
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = ExampleApp()
-    # window.response()
-    window.show()
-    app.exec_()
+if __name__ == '__main__':        
+    root = Tk()
+    root.title("DEV")
+    root.geometry("400x300")
 
-if __name__ == '__main__':
-    main()
+    progressbar = ttk.Progressbar(orient="horizontal", variable=value_var)
+    progressbar.pack(side=BOTTOM, fill=X, padx=6, pady=6 )
+
+    btn_response_link = ttk.Button(text="TOKEN", command=token) # создаем кнопку из пакета ttk
+    btn_response_link.pack(anchor="se")
+
+    root.mainloop()
--- vk_save.py
+++ vk_save.py
@@ -1,10 +1,7 @@
 import requests, json, time, os
 import re
 import urllib.request
-from dotenv import load_dotenv
-# from main import ExampleApp
-
-CD_DIR = os.getenv('CD_DIR')
+from threading import Thread
 
 def url_api(dow, count, VK_USER_ID, VK_TOKEN):
 	try:
@@ -22,19 +19,20 @@ def url_api(dow, count, VK_USER_ID, VK_TOKEN):
 	# return api, json.loads(api.text)
 
 def info_max_count(response_jsons):
-	js = response_jsons
-	return js['response']['count']
+	try:
+		return response_jsons['response']['count']
+	except:
+		print(response_jsons['error'])
 
-def test_api(response_json):
+def test_api_json(response_json):
 	try:
 		suum = []
-		i = 0 
+		i = 0
 		Mass_full_name = []
 		for files in response_json['response']['items']:
 			for WriteHeight in files['sizes']:
 				suum.append(WriteHeight["height"])
 			i=i+1
-
 			Url_IMG = files["sizes"][suum.index(max(suum))]["url"]
 			sizing_img = files["sizes"][suum.index(max(suum))]["width"]	
 
@@ -45,44 +43,36 @@ def test_api(response_json):
 	except:
 		return False
 
-def download(response_json):
-	# try:
+def download(response_count_max, filepath):
 		suum = []
+		app = "/"
 		i = 0 
-		Mass_full_name = []
-		for files in response_json['response']['items']:
+		for files in response_count_max['response']['items']:
 			for WriteHeight in files['sizes']:
 				suum.append(WriteHeight["height"])
-			# тут нужно подумать как считать фотографии по одной и отправлять через return 
-			# тут должно i = 1
 			i=i+1
-			Url_IMG = files["sizes"][suum.index(max(suum))]["url"]
 
+			Url_IMG = files["sizes"][suum.index(max(suum))]["url"]
 			sizing_img = files["sizes"][suum.index(max(suum))]["width"]
+
 			Update_Url_IMG = Url_IMG.split("/")[-1]
 			NameIMG = Update_Url_IMG.split("?")[0]
-
-			#print("-", "NAME: ", NameIMG, " ",sizing_img, " ", "URL :" ,Url_IMG, )
-			time.sleep(1)
-			Mass_full_name.append(NameIMG)
+			try:
+				urllib.request.urlretrieve(Url_IMG, filepath + app + NameIMG)
+				time.sleep(2)
+				if os.path.exists(filepath + app + NameIMG):
+					print("файл найден_" , NameIMG)
+				else:
+					print("файл не найден_", NameIMG)
+					logs = open('logs.txt', 'w')
+					logs.write(Url_IMG + '\n' )
+					logs.close
+			except:
+				logs = open('logs.txt', 'w')
+				logs.write("urllib.error.URLError",Url_IMG + '\n' )
+				logs.close
 			suum = []
 		return i
-			# urllib.request.urlretrieve(Url_IMG, CD_DIR + NameIMG)
-			# time.sleep(1)
-			# if os.path.exists(CD_DIR + NameIMG):
-			# 	print("файл найден____" ,NameIMG)
-			# else:
-			# 	print("файл не найден")
-			# 	print("check logs")
-			# 	logs = open('URL_link_ERROR.txt', 'w')
-			# 	logs.write(Url_IMG + '\n')
-			# 	logs.close
-			# 	with1 = open('test.txt', 'w')
-			# 	for index in Mass_full_name:
-			# 		with1.write(index + '\n')
-			# 	print("OK")
-	# except:
-	# 	return False	
 
 # def brain():
 # 	while dow != 2179:
